8c5e540d83f9c7ecf833c77e86140b9ced56048e
george-tk <g.kyriacou017@gmail.com>
2024-09-21 09:10:36 +0100

    toggle terminal

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,23 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    direction = 'float',
+    mode = 'normal',
+  },
+  config = true,
+  keys = {
+    {
+      '<leader>t',
+      function()
+        return vim.cmd('ToggleTerm' .. vim.v.count1)
+      end,
+      desc = '[T]erminal',
+    },
+  },
+}
+
+-- to correctly work with venv make sure init script (ie. .zshrc) has the follwoing configeration
+-- if [ "$SHLVL" = 1]; then
+--    export PATH = (all path commands)
+-- fi
