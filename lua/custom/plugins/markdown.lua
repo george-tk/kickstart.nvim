7bc8c91cfdeec206ef6301d7f49b80ef7dac4418
george-tk <g.kyriacou017@gmail.com>
2024-10-03 22:13:21 +0100

    change auto complete mapping for note taking

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -35,7 +35,7 @@ return {
       --vim.keymap.set('i', '<tab>', '<cmd>AutolistTab<cr>')
       --vim.keymap.set('i', '<s-tab>', '<cmd>AutolistShiftTab<cr>')
       -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
-      vim.keymap.set('i', '<C-CR>', '<CR><cmd>AutolistNewBullet<cr>')
+      vim.keymap.set('i', '<CR>', '<CR><cmd>AutolistNewBullet<cr>', { buffer = true })
       --vim.keymap.set('n', 'o', 'o<cmd>AutolistNewBullet<cr>')
       --vim.keymap.set('n', 'O', 'O<cmd>AutolistNewBulletBefore<cr>')
       --vim.keymap.set('n', '<S-CR>', '<cmd>AutolistToggleCheckbox<cr><CR>')
@@ -52,8 +52,8 @@ return {
       -- functions to recalculate list on edit
       --vim.keymap.set('n', '>>', '>><cmd>AutolistRecalculate<cr>')
       --vim.keymap.set('n', '<<', '<<<cmd>AutolistRecalculate<cr>')
-      vim.keymap.set('n', 'dd', 'dd<cmd>AutolistRecalculate<cr>')
-      vim.keymap.set('v', 'd', 'd<cmd>AutolistRecalculate<cr>')
+      vim.keymap.set('n', 'dd', 'dd<cmd>AutolistRecalculate<cr>', { buffer = true })
+      vim.keymap.set('v', 'd', 'd<cmd>AutolistRecalculate<cr>', { buffer = true })
     end,
   },
 }
--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -26,15 +26,16 @@ return {
   },
   opts = {
     sources = { 'filesystem', 'document_symbols' },
-    filtered_items = {
-      visible = true,
-    },
-    follow_current_file = {
-      enabled = true, -- This will find and focus the file in the active buffer every time
-      --               -- the current file is changed while the tree is open.
-      leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-    },
     filesystem = {
+
+      filtered_items = {
+        visible = true,
+      },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
       window = {
         mappings = {
           -- ['h'] = 'close_node',
