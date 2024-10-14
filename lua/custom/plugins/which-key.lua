b9a31aac42dc3333398b306d86fd316eedda7c9a
george-tk <g.kyriacou017@gmail.com>
2024-10-14 10:00:39 +0100

    fix which-key bug

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,9 +1,7 @@
 return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
-  keys = { '<leader>', '<c-r>', '<c-w>', '"', "'", '`', 'c', 'v', 'g' },
-  cmd = 'WhichKey',
-  event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
-  pts = {
+  event = 'VeryLazy',
+  opts = {
     icons = {
       -- set icon mappings to true if you have a Nerd Font
       mappings = vim.g.have_nerd_font,
