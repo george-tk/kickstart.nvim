6ad47f8ccf7575cd99e137735ab83f966b197628
root <root@UKEF-7PBS6M3.>
2024-11-01 11:47:39 +0000

    change markdown link format

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -69,17 +69,11 @@ return {
     opts = {
       mappings = {
         MkdnEnter = { { 'n', 'v', 'i' }, '<CR>', { buffer = true } },
-        MkdnTab = false,
-        MkdnSTab = false,
         MkdnNextLink = { 'n', '<Tab>', { buffer = true } },
         MkdnPrevLink = { 'n', '<S-Tab>', { buffer = true } },
-        MkdnNextHeading = { 'n', ']]' },
-        MkdnPrevHeading = { 'n', '[[' },
         MkdnGoBack = { 'n', '<BS>', { buffer = true } },
         MkdnGoForward = { 'n', '<Del>', { buffer = true } },
-        MkdnCreateLink = false, -- see MkdnEnter
         MkdnCreateLinkFromClipboard = { { 'n', 'v' }, '<leader>mp', { buffer = true, desc = '[M]arkdown [P]aste link' } }, -- see MkdnEnter
-        MkdnFollowLink = false, -- see MkdnEnter
         MkdnDestroyLink = { 'n', '<leader>md', { buffer = true, desc = '[M]arkdown [D]elete link' } },
         MkdnTagSpan = false,
         MkdnMoveSource = { 'n', '<leader>mm', { buffer = true, desc = '[M]arkdown [Move] link' } },
@@ -88,15 +82,11 @@ return {
         MkdnIncreaseHeading = false,
         MkdnDecreaseHeading = false,
         MkdnToggleToDo = { { 'n', 'v' }, '<leader>mt', { buffer = true, desc = '[M]arkdown [T]odo' } },
-        MkdnNewListItem = false,
         MkdnNewListItemBelowInsert = false,
         MkdnNewListItemAboveInsert = false,
-        MkdnExtendList = false,
         MkdnUpdateNumbering = { 'n', '<leader>mu', { buffer = true, desc = '[M]arkdown [U]pdate numbering' } },
         MkdnTableNextCell = { 'i', '<Tab>', { buffer = true } },
         MkdnTablePrevCell = { 'i', '<S-Tab>', { buffer = true } },
-        MkdnTableNextRow = false,
-        MkdnTablePrevRow = { 'i', '<M-CR>', { buffer = true } },
         MkdnTableNewRowBelow = { 'n', '<leader>mr', { buffer = true, desc = '[M]arkdown [r]ow below' } },
         MkdnTableNewRowAbove = { 'n', '<leader>mR', { buffer = true, desc = '[M]arkdown [R]ow above' } },
         MkdnTableNewColAfter = { 'n', '<leader>mc', { buffer = true, desc = '[M]arkdown [c]olumn after' } },
@@ -104,6 +94,15 @@ return {
         MkdnFoldSection = false, --{ 'n', '<leader>mf', { buffer = true } },
         MkdnUnfoldSection = false, --{ 'n', '<leader>mF', { buffer = true } },
       },
+      links = {
+        transform_explicit = function(text)
+          text = text:gsub(" ", "")
+          text = text:lower()
+          folder = 'unorganised/'
+          text = folder..os.date('%d-%m-%Y_')..text
+          return(text)
+        end,
+      }
     },
   },
 }
