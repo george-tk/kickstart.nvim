1e479e8bc09303466b0697ddd943560d6168b013
George Kyriacou <g.kyriacou017@gmail.com>
2025-01-02 10:49:00 +0000

    which-key delay decreased

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -2,6 +2,7 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VeryLazy',
   opts = {
+    delay = 0,
     icons = {
       -- set icon mappings to true if you have a Nerd Font
       mappings = false,
